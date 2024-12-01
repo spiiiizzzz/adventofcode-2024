149b14d5b4a404560a700e1ea41ee45a5a467cd6
Salvatore Pizzolanti <spizzolanti@proton.me>
2024-12-01 14:19:30 +0100

    day 1

---
+++ day1/day1-p2.py
@@ -0,0 +1,29 @@
+def presence_score(n: int, l: [int]) -> int:
+    presence = 0
+    for i in l:
+        if n == i:
+            presence += n
+    return presence
+
+
+def get_lists(raw: str) -> (str):
+    list_1 = []
+    list_2 = []
+    for line in raw:
+        (a, b) = line.strip().split('   ')
+        list_1.append(int(a))
+        list_2.append(int(b))
+    return (list_1, list_2)
+
+
+def main():
+    with open("input.txt", "r") as f:
+        list_1, list_2 = get_lists(f.readlines())
+    similarity = 0
+    for i in range(0, len(list_1)):
+        similarity += presence_score(list_1[i], list_2)
+    print(similarity)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day1/day1.py
@@ -0,0 +1,32 @@
+#
+# A bit overengineered maybe
+# But whatever, it works quite nicely
+#
+
+def distance(a: int, b: int) -> int:
+    return abs(a-b)
+
+
+def get_lists(raw: str) -> (str):
+    list_1 = []
+    list_2 = []
+    for line in raw:
+        (a, b) = line.strip().split('   ')
+        list_1.append(int(a))
+        list_2.append(int(b))
+    list_1.sort()
+    list_2.sort()
+    return (list_1, list_2)
+
+
+def main():
+    with open("input.txt", "r") as f:
+        list_1, list_2 = get_lists(f.readlines())
+    total_distance = 0
+    for i in range(0, len(list_1)):
+        total_distance += distance(list_1[i], list_2[i])
+    print(total_distance)
+
+
+if __name__ == "__main__":
+    main()
